ae5134f47244a66eee11d3bfa94436714cf86f20
Chano <officialchano18@gmail.com>
2026-06-08 20:06:11 +0800

    Add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/City.png" />
+        <title>eRSBSA</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
